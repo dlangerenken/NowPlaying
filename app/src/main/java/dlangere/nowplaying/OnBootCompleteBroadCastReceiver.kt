5d33bff81fc05d44651bd098063a48535b838ec9
Daniel Langerenken <daniel.langerenken@oracle.com>
2017-11-18 17:26:18 -0800

    mismatch kt files

--- app/src/main/java/dlangere/nowplaying/OnBootCompleteBroadCastReceiver.kt
+++ app/src/main/java/dlangere/nowplaying/OnBootCompleteBroadCastReceiver.kt
@@ -8,7 +8,7 @@ import android.content.Context
 /**
  * Created by dlangere on 11/18/17.
  */
-class BroadcastReceiverOnBootComplete : BroadcastReceiver() {
+class OnBootCompleteBroadCastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action!!.equals(Intent.ACTION_BOOT_COMPLETED, ignoreCase = true)) {
